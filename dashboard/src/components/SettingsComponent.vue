5ee950f02f3f6261c9622428fe46310858cd2cc2
Kovács Dávid <150684439+KoDavid08@users.noreply.github.com>
2026-02-25 17:42:31 +0100

    sidebar beta

---
+++ dashboard/src/components/SettingsComponent.vue
@@ -0,0 +1,14 @@
+<script></script>
+<template>
+    <!-- 
+        pop up window with animation 
+            - language
+            - theme
+            - dev menu
+                - restart
+                - resetdata
+                - maybe algorithm type
+                
+    -->
+</template>
+<style scoped></style>
---
+++ dashboard/src/views/AboutPage.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+<template>
+    <p>Lorem ipsum dolor sit amet.</p>
+    <p>© kazincbarcikai tollfosztogato egylet 2026</p>
+</template>
+<style scoped>
+</style>
